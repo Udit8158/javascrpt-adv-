f790defb0e9080f1813dea9617cb0d9a7a06c9c5
Udit8158 <kunduudit8158059368@gmail.com>
2021-11-24 21:31:17 +0530

    add new project

--- projects/06_DragDrop/js/script.js
+++ projects/06_DragDrop/js/script.js
@@ -1,23 +1,36 @@
 console.log("Creating drag and drop utility");
 
-let whiteboxes = document.querySelectorAll('.whiteboxes');
+let whiteboxes = document.getElementsByClassName('whiteboxes');
+let firstBox = document.querySelector('.firstBox');
 let imagebox = document.querySelector('.imagebox');
+console.log(firstBox.firstElementChild)
 
-imagebox.addEventListener('drag',()=>{
-    console.log("drag");
-})
-imagebox.addEventListener('dragend',()=>{
-    console.log("dragend");
-})
-imagebox.addEventListener('dragleave',()=>{
+for (const whitebox of whiteboxes) {
+    console.log(whitebox)
+
+// imagebox.addEventListener('dragstart',()=>{
+//     console.log("dragstart");
+// })
+// imagebox.addEventListener('drag',()=>{
+//     console.log("drag");
+// })
+// imagebox.addEventListener('dragover',()=>{
+//     console.log("dragover");
+// })
+imagebox.addEventListener('dragleave',(e)=>{
     console.log("dragleave");
+    imagebox.style = "border : 2px solid black ; display:none";
+    e.target.style = "display:none"
 })
-imagebox.addEventListener('dragenter',()=>{
+// whitebox.addEventListener('dragend',()=>{
+//     console.log("dragend");
+// })
+//When entering to the another the box
+whitebox.addEventListener('dragenter',(e)=>{
     console.log("dragenter");
+    e.target.className = "appendClass"
 })
-imagebox.addEventListener('dragover',()=>{
-    console.log("dragover");
+whitebox.addEventListener('drop',()=>{
+    console.log("drop");
 })
-imagebox.addEventListener('dragstart',()=>{
-    console.log("dragstart");
-})
+}
